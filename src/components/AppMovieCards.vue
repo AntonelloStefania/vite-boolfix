131661596df878be856f4079c8ddd5ff5ba953f8
AntonelloStefania <antonelloste94@gmail.com>
2023-06-26 15:38:41 +0200

    [add] svg bandiere

--- src/components/AppMovieCards.vue
+++ src/components/AppMovieCards.vue
@@ -1,19 +1,30 @@
 <script>
+    
 
     export default {
         props:{
             MyMovies : Object
+        },
+        data(){
+           return{
+           }
         }
 
     }
+    
 </script>
 <template >
     <div class="cards movie-card" >
         <h1>{{ MyMovies.title }}</h1>
         <h6>{{ MyMovies.original_title }}</h6>
-        <span>{{ MyMovies.original_language }}</span>
+        <span>{{ MyMovies.original_language }}
+            <img :src="'../../public/country-squared/'+ MyMovies.original_language +'.svg'" alt="">
+        </span>
         <p>{{ MyMovies.overview }}</p>
         <span>{{ MyMovies.vote_average }}</span>
+       
+        
+        
     </div>
 </template>
 <style lang="scss" scoped>
@@ -23,6 +34,11 @@
    h1{ 
     font-size: 22px;
    }
+}
+
+img{
+    width: 10px;
+    height: 10px;
 }
 
     
