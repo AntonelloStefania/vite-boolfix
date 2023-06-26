7c662ebb44396685b21d780fd770036e6494608c
AntonelloStefania <antonelloste94@gmail.com>
2023-06-26 11:51:28 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+   
+  export default {
+    data() {
+        return {
+            
+           
+        }
+    },
+  }
+</script>
+<template >
+  <div>
+    <ul>
+        <li>we</li>
+        <li>we</li>
+        <li>we</li>
+    </ul>
+  </div>
+</template>
+<style lang="scss">
+@use '../styles/generals.scss' as*;
+@use '../styles/partials/variables' as *
+  
+</style>
